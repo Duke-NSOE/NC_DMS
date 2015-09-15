0f8cd274f171f6679a292163d61bc7a16314c02c
johnpfay <john.fay@duke.edu>
2015-09-15 17:09:56 -0400

    Now generates from ALLHUC8 csv file.

--- Scripts/HABMODEL_CreateSWDFile.py
+++ Scripts/HABMODEL_CreateSWDFile.py
@@ -4,6 +4,10 @@
 #  column listing the species/background along with columns for all the env vars. Records produced
 #  are limited to only catchments with recorded presences from any species in Endries' data.
 #
+# This tool requires the following files in the species sub folder before it can be run:
+#  - The ALLHUC8Records.csv
+#  - 
+#
 # Spring 2015
 # John.Fay@duke.edu
 
@@ -11,10 +15,8 @@ import sys, os, arcpy, csv
 arcpy.env.overwriteOutput = True
 
 # Input variables
-speciesTbl = arcpy.GetParameterAsText(0)   # SH_Correlations.csv file
-speciesName = arcpy.GetParameterAsText(1) # SpeciesOccurrences table
-resultsTbl = arcpy.GetParameterAsText(2)   # <species_name>_RedundantVars.html file
-stats_folder = arcpy.GetParameterAsText(3) # ResponseVars table
+speciesName = arcpy.GetParameterAsText(0)  # Name of species to process
+stats_folder = arcpy.GetParameterAsText(1) # Stats root folder name;
 
 ## ---Functions---
 def msg(txt,type="message"):
@@ -25,113 +27,105 @@ def msg(txt,type="message"):
         arcpy.AddWarning(txt)
     elif type == "error":
         arcpy.AddError(txt)
+               
+def checkFile(fileName):
+    #Checks whether file exists. Sends error and exits if not. 
+    if not os.path.exists(fileName):
+        msg("File {} does not exist.\nExiting.".format(fileName),"error")
+        sys.exit(1)
+    else:
+        return 
 
 ## ---SET SCRIPT VARIABLES---
 # Set the species folder 
 msg("Locating species stats folder in root folder")
 sppFolder = os.path.join(stats_folder,speciesName)
-if not os.path.exists(sppFolder):
-    msg("Species folder not found at {}".format(sppFolder),"error")
-    sys.exit(1)
+checkFile(sppFolder)
+    
+# Set the ALLHUC8Records.csv file
+msg("Locating HUC8 records file in species folder")
+huc8RecordsCSV = os.path.join(sppFolder,"ALLHUC8Records.csv")
+checkFile(huc8RecordsCSV)
+
 # Set the SHCorrelations.csv file
 msg("Locating species-habitat correlations file in species folder")
 varFilterCSV = os.path.join(sppFolder,"SH_Correlations.csv")
-if not os.path.exists(varFilterCSV):
-    msg("SHCorrelations.csv file not found","error")
-    sys.exit(1)
+checkFile(varFilterCSV)
+    
 # Set the RedundantVariables html file
 msg("Locating redundant variables file in species folder")
 varFilterHTML = os.path.join(sppFolder,"{}_RedundantVars.html".format(speciesName))
-if not os.path.exists(varFilterHTML):
-    msg("{}_RedundantVars.html file not found".format(speciesName),"error")
-    sys.exit(1)
+checkFile(varFilterHTML)
 
 # Output variable (derived)
 swdCSV = os.path.join(sppFolder,"{}_SWD.csv".format(speciesName))
-arcpy.SetParameterAsText(4,swdCSV) #Output SWD format CSV file to create
+arcpy.SetParameterAsText(2,swdCSV) #Output SWD format CSV file to create
 
-# temp varables
-sppOnlyTbl = "in_memory/SppTble"
-resultsCopyTbl = "in_memory/Results2"
-counter = 0
+## ------------------------Processes-----------------------
+#Create the list of fields to convey to the SWD CSV file
+msg("Building field list")
+inFldList = ["SPECIES","GRIDCODE","REACHCODE"]
 
-## ---Processes---
-# Extract Catchments with species
-msg("Pulling catchment records for {}".format(speciesName))
-arcpy.TableSelect_analysis(speciesTbl, sppOnlyTbl,'"{}" = 1'.format(speciesName))
-
-# Make a copy of the environment variable table and join the species table to it
-msg("...Creating temporary table of the environment variables")
-#arcpy.CopyRows_management(resultsTbl,resultsCopyTbl)
-arcpy.TableSelect_analysis(resultsTbl,resultsCopyTbl,"LENGTHKM > 0")
-
-# Remove uncorrelated fields
+#Get the list of significant variables from the SH_Correlations table and add them to the list
+msg("...Adding fields identified as significantly correlated with species presence")
 f = open(varFilterCSV,'rt')
-keepList = ["OBJECTID","GRIDCODE","FEATUREID"]
 for row in csv.reader(f):
-    keepList.append(row[0])
+    inFldList.append(row[0])
 f.close()
-fldList = arcpy.ListFields(resultsCopyTbl)
-for fld in fldList:
-    if not fld.name in keepList:
-        msg("   Removing <{}> (not correlated with presence)".format(fld.name))
-        arcpy.DeleteField_management(resultsCopyTbl,fld.name)
+inFldList.remove('variable') #Remove the column header from the list
+msg("...{} fields added".format(len(inFldList) - 3))
 
-# Remove redundant fields
-f = open(varFilterHTML, 'rt')       #open the file
-lineString = f.readline()           #get the info
-f.close()                           #close the file
-lineItems = lineString.split("<br>")[1:-1]
-fldList = arcpy.ListFields(resultsCopyTbl)
-for fld in fldList:
-    if fld.name in lineItems:
-        msg("   Removing <{}> (redundant)".format(fld.name))
-        arcpy.DeleteField_management(resultsCopyTbl,fld.name)
-        
+#Remove redundant fields
+msg("Removing fields identified as redundant")
+f = open(varFilterHTML, 'rt')               #open the file
+lineString = f.readline()                   #get the info
+f.close()                                   #close the file
+lineItems = lineString.split("<br>")[1:-1]  #Get the fields from the line
+fldList = list(inFldList)                #Make a clone of the outField list to loop through (so we can remove
+                                            # values from the outFieldList in the loop
+for fld in fldList:                         #Loop through fields; if it's in the lineItems list, it should be removed
+    if fld in lineItems:
+        msg("   Removing <{}> (redundant)".format(fld))
+        inFldList.remove(fld)
 
-# Join the species data to the results table so that the records where the species
-# is present can be isolated. 
-msg("...Joining species presence values to environment variables")
-arcpy.JoinField_management(resultsCopyTbl,"GRIDCODE",sppOnlyTbl,"GRIDCODE","{}".format(speciesName))
-
-# Create a list of field names
-msg("Generating a list of environment variables for processing") 
-fldList = [] #[speciesName] #start the list with the MaxEnt format: {species name, x, y}
-for fld in arcpy.ListFields(resultsCopyTbl):
-    fldList.append(str(fld.name))
+#Clone the inFldList to the outFldList and replace GRIDCODE and REACHCODE with "X" and "Y"
+outFldList = list(inFldList)
+outFldList.pop(1)
+outFldList.insert(1,"X")
+outFldList.pop(2)
+outFldList.insert(2,"Y")
 
 ## WRITE THE SPECIES RECORDS TO THE FILE ##
-msg("Initializing the output species file...")
 # Initialize the species output csv file & create the writer object
-msg("...Initializing the output CSV files")
+msg("Initializing the output CSV files")
 csvFile = open(swdCSV,'wb')
 writer = csv.writer(csvFile)
 
 # Write header row to CSV file
 msg("...Writing headers to CSV file")
-writer.writerow(["Species","X","Y"] + fldList[3:-1]) #<- the 3: is to skip the first three columns in the fldList
+writer.writerow(outFldList) 
 
-# Create a search cursor for the resultsTbl
-msg("...Writing presence values to CSV file")
-whereClause = '"{}" = 1'.format(speciesName)
+# Create a search cursor for the huc8 CSV
+msg("...Writing catchment records")
 # Create a cursor including all but the first and last fields (species names)
-cursor = arcpy.da.SearchCursor(resultsCopyTbl,fldList[1:-1],whereClause) #<- the 1: skips the first & last field
+cursor = arcpy.da.SearchCursor(huc8RecordsCSV,inFldList)
+# Initialize the counters
+presenceCounter = 0
+absenceCounter = 0
 for row in cursor:
+    rowData = list(row)
+    #If the species is present, set first column to the species name, otherwise background
+    if rowData[0] == 1:
+        rowData[0] = speciesName
+        presenceCounter += 1
+    else:
+        rowData[0] = "Background"
+        absenceCounter += 1
     #write the species name + all the row data
-    writer.writerow([speciesName] + list(row))
-    counter += 1
-msg("{} presence records writted to file".format(counter))
-counter = 0
+    writer.writerow(rowData)
 
-# Create a search cursor for the resultsTbl
-msg("...Writing background values to CSV file")
-whereClause = '"{}" IS Null'.format(speciesName)
-cursor = arcpy.da.SearchCursor(resultsCopyTbl,fldList[1:-1],whereClause)
-for row in cursor:
-    #write the species name + all the row data
-    writer.writerow(['background'] + list(row))
-    counter +=1
-msg("{} absence records writted to file".format(counter))
+msg("{} species records written to file".format(presenceCounter))
+msg("{} background records written to file".format(absenceCounter))
 
 # Close file and clean up
 csvFile.close()
