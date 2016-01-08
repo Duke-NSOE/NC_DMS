342c6808bc7c6f7c1cd459dbb797e85b328ec3c2
johnpfay <john.fay@duke.edu>
2016-01-08 16:49:22 -0500

    Add uplift ranks on nonzeros and uplift range

--- Scripts/UPLIFT_CalcUpliftMinMax.py
+++ Scripts/UPLIFT_CalcUpliftMinMax.py
@@ -35,7 +35,8 @@ def checkFile(fileName):
 #Create list of spp uplift fields. Also, check whether the fields already exist; exit if so
 upliftFlds = []
 for fld in arcpy.ListFields(upliftTable):
-    if fld.name == "minUplift" or fld.name == "maxUplift":
+    print fld.name
+    if fld.name == "MinUplift" or fld.name == "MaxUplift":
         msg("{} already exists for this table. Exiting".format(fld.name),"error")
         sys.exit(0)
     elif fld.name[-3:] == "_up":
@@ -57,12 +58,57 @@ arcpy.AddField_management(upliftTable,minFldName,"DOUBLE")
 msg("...adding max uplift field")          
 maxFldName = "MaxUplift"
 arcpy.AddField_management(upliftTable,maxFldName,"DOUBLE")
+msg("...adding uplift range field")          
+rngFldName = "UpliftRange"
+arcpy.AddField_management(upliftTable,rngFldName,"DOUBLE")
 
 #Apply the calcStrings
 msg("...calculating min likelihood")
 arcpy.CalculateField_management(upliftTable,minFldName,minCalcStr,"PYTHON")
 msg("...calculating max likelihood")
 arcpy.CalculateField_management(upliftTable,maxFldName,maxCalcStr,"PYTHON")
+msg("...calculating uplift range")
+rangeCalcStr = "!{}! - !{}!".format(maxFldName,minFldName)
+arcpy.CalculateField_management(upliftTable,rngFldName,rangeCalcStr,"PYTHON")
 
+##Compute deciles on uplift values
+#Get the field name of the average uplift values
+avgFldName = "MeanUplift"
+
+#Add the new field
+msg("Computing uplift deciles on non zero scores")
+msg("...adding uplift decile field")          
+up2FldName = "UpRank2"
+arcpy.AddField_management(upliftTable,up2FldName,"SHORT")
+
+#Set the where clause
+lowerBound = float("0") 
+upperBound = float("0")
+whereClause = "{0} < {1} OR {0} > {2}".format(avgFldName,lowerBound,upperBound)
+selFC = arcpy.MakeFeatureLayer_management(upliftTable,"Lyr",whereClause)
+
+#Get the number of records and determine the quantile size
+numRecs = int(arcpy.GetCount_management(selFC).getOutput(0))
+msg("...{} Records used".format(numRecs))
+decileSize = numRecs / 10.0
+#Set the counter variables
+decile = 1              #Index of decile's upper limit
+ceiling = decileSize    #Initial decile value
+counter = 1             #Counter that increases with each record
+
+#Create the update cursor, sorted on average uplift
+records = arcpy.UpdateCursor(selFC,whereClause,"","{}; {}".format(avgFldName,up2FldName),"{} A".format(avgFldName))
+rec = records.next()
+while rec:
+    #Check to see if we've entered a new decile, if so, increase the index
+    if counter > ceiling:       #If the current rec passes the ceiling
+        ceiling += decileSize   #...raise the ceiling
+        decile += 1           #...up the decile value
+    #Assign the quantile to the rank field
+    rec.setValue(up2FldName,decile)
+    records.updateRow(rec)
+    #Move to the next record
+    counter += 1
+    rec = records.next()
 #Finished
 msg("Completed")
